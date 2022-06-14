3fd792dbdedc2c7be21d5c79c24c62be11018aed
Foxtrot47 <jjneutron@outlook.com>
2022-06-14 12:28:39 +0530

    Use Source Serif Pro and Source Sans Pro as default font
    * for now

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,16 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 module.exports = {
   content: [
     "./src/**/*.{js,jsx,ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      fontFamily: {
+        'serif': ['Source Serif Pro', ...defaultTheme.fontFamily.serif],
+        'sans': ['Source Sans Pro', ...defaultTheme.fontFamily.sans],
+      },
+    },
   },
   plugins: [],
 }
