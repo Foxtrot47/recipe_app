22f570c75ef225694a238a09a89437513aced4ea
Foxtrot47 <jjneutron@outlook.com>
2022-07-17 12:31:49 +0530

    Fetch data from local JSON server
    * I don't wanna spam api requests to real backend and drain Quota

---
+++ src/Helpers.js
@@ -0,0 +1,8 @@
+import axios from "axios";
+
+async function fetchData(callback) {
+    await axios.get("http://localhost:3000/recipes/")
+        .then(response => callback(response))
+        .catch(error => callback(error));
+}
+export default fetchData;
