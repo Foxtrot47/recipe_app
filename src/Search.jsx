ee1c32eba58fea44e1fd1934aa98f45627d7986a
Foxtrot47 <jjneutron@outlook.com>
2022-08-12 12:20:33 +0530

    Finish up basic search page with all filters

--- src/Search.jsx
+++ src/Search.jsx
@@ -1,6 +1,8 @@
 import Navbar from "./Navbar.jsx";
 import { useEffect, useState } from "react";
-
+import { useSearchParams, Link } from "react-router-dom";
+import { fetchData, renderRating } from "./Helpers.jsx";
+import serialize from "form-serialize";
 import {
   filters,
   dietTypes,
@@ -16,26 +18,48 @@ import {
 const Recipe = () => {
   const [results, setResults] = useState(null);
   const [loading, setLoading] = useState(true);
+  const [searchParams, setSearchParams] = useSearchParams();
 
-  const searchParams = new URLSearchParams(location.search);
   useEffect(() => {
-    if (searchParams === undefined) return;
-    let endPoint = "search";
-    let body = {};
-    searchParams.forEach((param, key) => {
-      body[key] = param;
+    const body = {};
+    let params = new URLSearchParams(searchParams);
+    for (const [key, value] of params.entries()) {
+      body[key] = value;
+    }
+    // Copy the search query to a hidden input field so that it will not get removed on parameter change
+    // This is because search query intially came from outside the form and serialize function will not see it
+    document.getElementById("queryField").value = body["q"];
+    fetchData("search", body, "get", (response) => {
+      if (response.status === 200 && response.data !== null) {
+        setResults(response.data);
+        setLoading(false);
+      } else {
+        console.log("Fetching resuls from API failed");
+      }
     });
   }, [searchParams]);
+
+  const handleSubmit = () => {
+    const data = serialize(document.getElementById("filters"), { hash: true });
+    setSearchParams(data);
+  };
 
   return (
     <div className="">
       <Navbar />
-      <div className="flex flex-col gap-y-6 mt-14">
-        <form className="flex flex-row drop-shadow text-black bg-white px-10 justify-around relative">
+      <div className="flex flex-col mt-14">
+        <form
+          id="filters"
+          className="fixed w-full flex flex-row drop-shadow text-black bg-white px-10 justify-around z-10"
+          onClick={handleSubmit}
+        >
+          {/* Hidden Input field to keep search query intact*/}
+          <input type="text" id="queryField" name="q" className="hidden" />
           {Object.keys(filters).map((i) => {
             return (
               <div
                 key={i}
+                s
                 className={`flex flex-row items-center gap-x-2 group py-5 px-6 ${
                   !filters[i]["full-width"] && "relative"
                 }`}
@@ -43,7 +67,7 @@ const Recipe = () => {
                 {filters[i].name}
                 <i className="fa-solid fa-angle-down group-hover:rotate-180 transition ease-out duration-300"></i>
                 <div
-                  className={`hidden group-hover:inline-block absolute top-[65px] bg-white drop-shadow-lg py-8 w-72
+                  className={`hidden group-hover:inline-block absolute top-[65px] bg-white drop-shadow-lg py-8 w-72 z-50
                     ${filters[i]["full-width"] && "w-screen"} ${
                     (filters[i].name === "Servings" && "right-0 w-56") ||
                     "left-0"
@@ -62,7 +86,9 @@ const Recipe = () => {
                             <input
                               type="checkbox"
                               name="mealtypes"
+                              value={type}
                               className="w-5"
+                              onClick={handleSubmit}
                             />
                             <p>{type}</p>
                           </div>
@@ -75,7 +101,9 @@ const Recipe = () => {
                               <input
                                 type="checkbox"
                                 name="totalTime"
+                                value={index}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{timeFilters[index]}</p>
                             </div>
@@ -88,7 +116,9 @@ const Recipe = () => {
                               <input
                                 type="checkbox"
                                 name="dietType"
+                                value={diet}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{diet}</p>
                             </div>
@@ -100,8 +130,10 @@ const Recipe = () => {
                             <div key={i} className="flex flex-row gap-x-4">
                               <input
                                 type="checkbox"
-                                name="maxCalorie"
+                                name="kcal"
+                                value={index}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{calorieRange[index]}</p>
                             </div>
@@ -113,8 +145,10 @@ const Recipe = () => {
                             <div key={i} className="flex flex-row gap-x-4">
                               <input
                                 type="checkbox"
-                                name="difficulty"
+                                name="skillLevel"
+                                value={difficulty}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{difficulty}</p>
                             </div>
@@ -127,7 +161,9 @@ const Recipe = () => {
                               <input
                                 type="checkbox"
                                 name="cuisine"
+                                value={cuisine}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{cuisine}</p>
                             </div>
@@ -140,7 +176,9 @@ const Recipe = () => {
                               <input
                                 type="checkbox"
                                 name="rating"
+                                value={i}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{ratingRanges[i]}</p>
                             </div>
@@ -152,8 +190,10 @@ const Recipe = () => {
                             <div key={i} className="flex flex-row gap-x-4">
                               <input
                                 type="checkbox"
-                                name="servings"
+                                name="yield"
+                                value={serving}
                                 className="w-5"
+                                onClick={handleSubmit}
                               />
                               <p>{serving}</p>
                             </div>
@@ -165,6 +205,37 @@ const Recipe = () => {
             );
           })}
         </form>
+        <div className="grid grid-cols-4 gap-10 mt-20 px-24">
+          {!loading &&
+            results.length > 0 &&
+            results.map((recipe, id) => {
+              return (
+                <Link
+                  key={id}
+                  to={"/recipes/" + recipe.slug}
+                  className="flex flex-col gap-y-6 hover:bg-gray-50 rounded-lg hover:drop-shadow-lg"
+                >
+                  <img
+                    className="drop-shadow-xl filter rounded-lg object-cover object-center h-56"
+                    src={recipe.image.url}
+                    alt={recipe.image.alt}
+                  />
+                  <div className="flex flex-col gap-y-2 px-4">
+                    <p className="text-xl font-semibold truncate w-full">
+                      {recipe.name}
+                    </p>
+                    <div className="flex flex-row gap-x-2 text-accent">
+                      {renderRating(recipe.rating.avg)}
+                    </div>
+                    <div className="flex flex-row gap-x-2 text-sm"></div>
+                    <p className="h-12 mb-4 w-full text-ellipsis overflow-hidden">
+                      {recipe.description}
+                    </p>
+                  </div>
+                </Link>
+              );
+            })}
+        </div>
       </div>
     </div>
   );
