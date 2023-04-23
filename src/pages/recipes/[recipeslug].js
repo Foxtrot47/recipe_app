b762c403be343f457ab2872541aeb05a1747b330
Foxtrot47 <jjneutron@outlook.com>
2023-04-23 23:20:46 +0530

    Recipe Page migration

--- src/pages/recipes/[recipeslug].js
+++ src/pages/recipes/[recipeslug].js
@@ -1,63 +1,63 @@
-import { useState, useEffect } from 'react'
-import { useParams, Link } from 'react-router-dom'
+import { useState, useEffect } from "react";
+import Link from "next/link";
+import { useRouter } from "next/router";
 
-import axios from 'axios'
-import Icon from '@mdi/react'
-import { mdiBarleyOff, mdiEggOff, mdiSquareCircle } from '@mdi/js'
-import moment from 'moment'
-import { fetchData, renderRating } from '@/Helpers'
-import { mealTypesSmall } from '@/SearchData'
+import axios from "axios";
+import moment from "moment";
+import { fetchData, renderRating } from "@/Helpers";
+import { mealTypesSmall } from "@/SearchData";
 
 const Recipe = () => {
-  const [recipeData, setRecipeData] = useState(null)
-  const [reviews, setReviews] = useState(null)
-  const [loading, setLoading] = useState(true)
-  const { recipeslug } = useParams()
+  const [recipeData, setRecipeData] = useState(null);
+  const [reviews, setReviews] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const router = useRouter();
+  const { recipeslug } = router.query;
 
   useEffect(() => {
-    if (recipeslug === undefined) return
-    fetchData('recipebyslug', { slug: recipeslug }, 'get', (response) => {
+    if (recipeslug === undefined) return;
+    fetchData("recipebyslug", { slug: recipeslug }, "get", (response) => {
       if (
         response.status === 200 &&
         response.data !== null &&
         response.data.length > 0
       ) {
-        setRecipeData(response.data[0])
+        setRecipeData(response.data[0]);
         // The key is _id and not id because of the way mongodb stores data
         // eslint-disable-next-line no-underscore-dangle
-        fetchReviews(response.data[0]._id)
-        setLoading(false)
+        fetchReviews(response.data[0]._id);
+        setLoading(false);
       } else {
-        console.log('uh oh')
+        console.log("uh oh");
       }
-    })
-  }, [recipeslug])
+    });
+  }, [recipeslug]);
 
   const fetchReviews = async (recipeId) => {
     // Required params for comments api
     const params = {
-      siteId: 'bbcgoodfood',
-      entityType: 'recipe',
+      siteId: "bbcgoodfood",
+      entityType: "recipe",
       entityId: recipeId,
-      source: 'content-api',
+      source: "content-api",
       itemsPerPage: 5,
       page: 1,
-      client: 'bbcgoodfood',
-    }
+      client: "bbcgoodfood",
+    };
     await axios({
-      method: 'get',
-      url: 'https://reactions.api.immediate.co.uk/api/reactions?',
+      method: "get",
+      url: "https://reactions.api.immediate.co.uk/api/reactions?",
       params,
     }).then((response) => {
       if (
         response.status === 200 &&
         response.data !== null &&
-        response.data['hydra:member'].length > 0
+        response.data["hydra:member"].length > 0
       ) {
-        setReviews(response.data['hydra:member'])
+        setReviews(response.data["hydra:member"]);
       }
-    })
-  }
+    });
+  };
 
   return (
     <div className="flex flex-col gap-y-6 mt-14">
@@ -73,14 +73,14 @@ const Recipe = () => {
           {!loading && recipeData.name}
         </div>
         <div className="text-lg md:flex md:flex-row gap-x-2 items-center z-0 text-gray-300 font-regular">
-          <Link to="/" className="hover:text-gray-100">
+          <Link href="/" className="hover:text-gray-100">
             <i className="fa-solid fa-home"></i>
             &nbsp;Home&nbsp;
             <i className="fa-regular fa-angle-right text-sm"></i>
           </Link>
           {!loading && recipeData.category[0] && (
             <Link
-              to={`/search?mealTypes=${recipeData.category[0]}`}
+              href={`/search?mealTypes=${recipeData.category[0]}`}
               className="hover:text-gray-100"
             >
               &nbsp;
@@ -93,7 +93,7 @@ const Recipe = () => {
         </div>
       </div>
       <div className="flex flex-col md:flex-row gap-x-6 px-4 md:px-20">
-        <div className="flex flex-col gap-y-1 ">
+        <div className="flex flex-col gap-y-1">
           <div className="flex flex-col md:flex-row gap-y-2 md:gap-x-6 p-2 mb-1">
             {recipeData && recipeData.author && (
               <div className="text-gray-500 dark:text-gray-100 flex flex-row gap-x-1 items-center">
@@ -129,24 +129,33 @@ const Recipe = () => {
                     key={dietInfo.id}
                     className="flex flex-row gap-x-2 items-center whitespace-normal"
                   >
-                    {(dietInfo.slug === 'diary-free' && (
-                      <Icon path={mdiEggOff} title="Diary free" />
+                    {(dietInfo.slug === "diary-free" && (
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path d="M22.1 21.5L2.4 1.7L1.1 3L6.1 8C5.1 10.3 4.5 12.8 4.5 14.5C4.5 18.6 7.9 22 12 22C14.2 22 16.2 21 17.6 19.5L20.8 22.7L22.1 21.5M19.5 14.5C19.5 10.4 16.1 2 12 2C10.5 2 9.1 3.1 7.9 4.7L19.3 16.1C19.4 15.6 19.5 15.1 19.5 14.5Z" />
+                      </svg>
                     )) ||
-                      (dietInfo.slug === 'gluten-free' && (
-                        <Icon
-                          path={mdiBarleyOff}
-                          title="Gluten free"
-                          size="19"
+                      (dietInfo.slug === "gluten-free" && (
+                        <svg
+                          xmlns="http://www.w3.org/2000/svg"
+                          viewBox="0 0 24 24"
                           className="text-red-500"
-                        />
+                          stroke="currentColor"
+                          fontSize={19}
+                        >
+                          <path d="M11.96,1.21C11.3,2.76 10.64,4.31 10.65,5.82C10.66,6.42 10.77,7 10.94,7.62C10.86,7.46 10.77,7.31 10.67,7.17C9.83,6 8.17,5 6.5,4C6.5,4.8 6.5,5.59 6.68,6.36L13,12.68V10.7C14.5,9.8 15.92,8.88 16.67,7.83C17.5,6.67 17.5,5.33 17.5,4C15.83,5 14.17,6 13.33,7.17C13.23,7.31 13.15,7.45 13.07,7.59C13.25,6.96 13.36,6.32 13.35,5.69C13.34,4.18 12.65,2.69 11.96,1.21M3.28,5.5L2,6.77L6.64,11.41C6.75,12 6.95,12.55 7.33,13.08C8.08,14.13 9.5,15.05 11,15.95V18.23L10.67,17.67C9.83,16.5 8.17,15.5 6.5,14.5C6.5,15.83 6.5,17.17 7.33,18.33C8.08,19.38 9.5,20.3 11,21.2V23H13V21.2C13.74,20.76 14.45,20.31 15.07,19.84L18.73,23.5L20,22.22C14,16.23 9.1,11.32 3.28,5.5M17.5,9.25C15.83,10.25 14.17,11.25 13.33,12.42L13.12,12.79L15,14.66C15.67,14.16 16.27,13.64 16.67,13.08C17.5,11.92 17.5,10.58 17.5,9.25M17.5,14.5C16.93,14.84 16.38,15.18 15.85,15.53L17.29,16.97C17.5,16.17 17.5,15.33 17.5,14.5Z" />
+                        </svg>
                       )) ||
-                      (dietInfo.slug === 'vegan' && (
+                      (dietInfo.slug === "vegan" && (
                         <i className="fa-solid fa-circle-check text-red-500"></i>
                       )) ||
-                      (dietInfo.slug === 'healthy' && (
+                      (dietInfo.slug === "healthy" && (
                         <i className="fa-solid fa-heart text-red-500"></i>
                       ))}
-                    {dietInfo.display !== 'Vegetarian' && dietInfo.display}
+                    {dietInfo.display !== "Vegetarian" && dietInfo.display}
                   </div>
                 ))}
               </div>
@@ -163,17 +172,25 @@ const Recipe = () => {
               <div className="flex flex-row gap-x-6 absolute bottom-3 md:bottom-5 left-3 md:left-5 y-0 bg-white">
                 {recipeData.diet.find(
                   (term) =>
-                    term.display === 'Vegetarian' || term.display === 'Vegan',
+                    term.display === "Vegetarian" || term.display === "Vegan"
                 ) ? (
-                  <Icon
-                    path={mdiSquareCircle}
-                    className="text-[#008100] w-10 md:w-12"
-                  />
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
+                    className="text-green-veg w-10 md:w-12"
+                    fill="currentColor"
+                  >
+                    <path d="M20 4V20H4V4H20M22 2H2V22H22V2M12 6C8.69 6 6 8.69 6 12S8.69 18 12 18 18 15.31 18 12 15.31 6 12 6Z" />
+                  </svg>
                 ) : (
-                  <Icon
-                    path={mdiSquareCircle}
+                  <svg
+                    xmlns="http://www.w3.org/2000/svg"
+                    viewBox="0 0 24 24"
                     className="text-[#9d380c] w-10 md:w-12"
-                  />
+                    fill="currentColor"
+                  >
+                    <path d="M20 4V20H4V4H20M22 2H2V22H22V2M12 6C8.69 6 6 8.69 6 12S8.69 18 12 18 18 15.31 18 12 15.31 6 12 6Z" />
+                  </svg>
                 )}
               </div>
               <div className="absolute top-3 md:top-5 left-3 md:left-5 border border-black bg-white px-2 py-1">
@@ -189,7 +206,7 @@ const Recipe = () => {
               <div className="flex flex-col">
                 <span className="text-xl font-semibold">Difficulty</span>
                 <span className="text-gray-400">
-                  {(!loading && recipeData.skillLevel) || 'N/A'}
+                  {(!loading && recipeData.skillLevel) || "N/A"}
                 </span>
               </div>
             </div>
@@ -198,7 +215,7 @@ const Recipe = () => {
               <div className="flex flex-col">
                 <span className="text-xl font-semibold">Servings</span>
                 <span className="text-gray-400">
-                  {(!loading && recipeData.yield) || 'N/A'}
+                  {(!loading && recipeData.yield) || "N/A"}
                 </span>
               </div>
             </div>
@@ -333,7 +350,7 @@ const Recipe = () => {
                             <div className="text-lg text-gray-500">
                               {moment(
                                 review.changed,
-                                'YYYY-MM-DDTh:mm:ss a',
+                                "YYYY-MM-DDTh:mm:ss a"
                               ).fromNow()}
                             </div>
                           )}
@@ -367,7 +384,7 @@ const Recipe = () => {
                 <Link
                   key={recipe.id}
                   className="flex flex-row gap-x-4 w-full pr-6 rounded-lg hover:bg-gray-100 dark:hover:bg-gray-600 hover:drop-shadow"
-                  to={recipe.url}
+                  href={recipe.url}
                 >
                   <div className="flex-none">
                     <img
@@ -399,7 +416,7 @@ const Recipe = () => {
                 <Link
                   key={category.id}
                   className="flex flex-row gap-x-2"
-                  to={`/search?mealTypes=${category}`}
+                  href={`/search?mealTypes=${category}`}
                 >
                   <i className="fa-solid fa-circle-arrow-right text-red-500"></i>
                   <p> {category}</p>
@@ -410,7 +427,7 @@ const Recipe = () => {
         </div>
       </div>
     </div>
-  )
-}
+  );
+};
 
-export default Recipe
+export default Recipe;
